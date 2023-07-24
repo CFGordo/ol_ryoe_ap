86859e5208510ffc1e62a7344c4ac0a63d350395
CFGordo <111022581+CFGordo@users.noreply.github.com>
2023-07-24 15:52:55 -0400

    Player

--- main.py
+++ main.py
@@ -11,34 +11,36 @@ ol_ryoe_csv = ol_ryoe_link.replace('/edit#gid=', '/export?format=csv&gid=')
 ol_ryoe = pd.read_csv(ol_ryoe_csv)
 
 
-exes =['RYOE', 'Run Blocking Snaps',
+exes =[
+       'Team RYOE', 'Team Attempts', 'Team AVG RYOE', 'Team EPA', 'Team AVG EPA',
+       'Team Success Rate', 'Team 1st&10 Attempts', 'Team 1st&10 AVG RYOE',
+       'Team 1st&10 AVG EPA', 'Team 1st&10 Success Rate',
+       'TM_short_att', 'Team Short AVG RYOE', 'Team Short AVG EPA',
+       'Team Short Conversion Rate', 'Team Power Attempts', 'Team Power AVG RYOE',
+       'Team Power AVG EPA', 'Team Power Conversion Rate',
+       'Run Blocking Snaps', 'RYOE',
        'AVG RYOE', 'EPA', 'AVG EPA', 'Success Rate', '1st&10 Snaps',
        '1st&10 AVG RYOE', '1st&10 AVG EPA',
        '1st&10 Success Rate', 'Short Snaps',
        'Short AVG RYOE', 'Short AVG EPA', 'Short Conversion Rate',
        'Power Snaps', 'Power AVG RYOE', 'Power AVG EPA',
-       'Power Conversion Rate', 'Team Attempts',
-       'Team RYOE', 'Team AVG RYOE', 'Team EPA', 'Team AVG EPA',
+       'Power Conversion Rate'
+       ]
+
+texas=['Team EPA', 'Team Attempts',
+       'Team RYOE', 'Team AVG RYOE',  'Team AVG EPA',
        'Team Success Rate', 'Team 1st&10 Attempts', 'Team 1st&10 AVG RYOE',
        'Team 1st&10 AVG EPA', 'Team 1st&10 Success Rate',
        'TM_short_att', 'Team Short AVG RYOE', 'Team Short AVG EPA',
        'Team Short Conversion Rate', 'Team Power Attempts', 'Team Power AVG RYOE',
-       'Team Power AVG EPA', 'Team Power Conversion Rate'
-       ]
-
-texas=['EPA', 'Run Blocking Snaps',
-       'AVG RYOE', 'RYOE', 'AVG EPA', 'Success Rate', '1st&10 Snaps',
+       'Team Power AVG EPA', 'Team Power Conversion Rate',
+       'Run Blocking Snaps', 'RYOE',
+       'AVG RYOE', 'EPA', 'AVG EPA', 'Success Rate', '1st&10 Snaps',
        '1st&10 AVG RYOE', '1st&10 AVG EPA',
        '1st&10 Success Rate', 'Short Snaps',
        'Short AVG RYOE', 'Short AVG EPA', 'Short Conversion Rate',
        'Power Snaps', 'Power AVG RYOE', 'Power AVG EPA',
-       'Power Conversion Rate', 'Team Attempts',
-       'Team RYOE', 'Team AVG RYOE', 'Team EPA', 'Team AVG EPA',
-       'Team Success Rate', 'Team 1st&10 Attempts', 'Team 1st&10 AVG RYOE',
-       'Team 1st&10 AVG EPA', 'Team 1st&10 Success Rate',
-       'TM_short_att', 'Team Short AVG RYOE', 'Team Short AVG EPA',
-       'Team Short Conversion Rate', 'Team Power Attempts', 'Team Power AVG RYOE',
-       'Team Power AVG EPA', 'Team Power Conversion Rate'
+       'Power Conversion Rate'
        ]
 
 ybox = st.selectbox('Select Y Axis!', texas)
