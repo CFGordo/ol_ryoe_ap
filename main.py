e382630df4b99de4d7922c204de605239bb06565
CFGordo <111022581+CFGordo@users.noreply.github.com>
2023-07-24 13:43:18 -0400

    slider

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ xbox = st.selectbox('Select X Axis!', exes)
 games = st.slider('Run Snaps Filter', 1, 300, 100)
 ol_ryoe = ol_ryoe.loc[ol_ryoe['Run Blocking Snaps'] >= games]
 
-year = st.slider('Season Filter', value=[2016, 2022])
+year = st.slider('Season Filter', 2016, 2022, (2016, 2022))
 #ol_ryoe = ol_ryoe.loc[ol_ryoe['Season'] >= year.min()]
 
 for xbrick in exes:
