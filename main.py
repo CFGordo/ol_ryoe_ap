156ba8263ad967f5a1d460b0d10f96d078116e79
CFGordo <111022581+CFGordo@users.noreply.github.com>
2023-07-25 12:38:32 -0400

    marker logos

--- main.py
+++ main.py
@@ -47,6 +47,7 @@ gif_link = st.secrets["gif_url"]
 st.image(gif_link)
 
 logo_link = st.secrets["logo_url"]
+st.image(logo_link, "NE.png")
 
 ybox = st.selectbox('Select Y Axis!', texas)
 xbox = st.selectbox('Select X Axis!', exes)
