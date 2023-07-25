f770a31a318f472bcfcfc14654c43ce5ba8ad1de
CFGordo <111022581+CFGordo@users.noreply.github.com>
2023-07-25 12:29:13 -0400

    marker logos

--- main.py
+++ main.py
@@ -43,7 +43,8 @@ texas=['Team EPA', 'Team Attempts',
        'Power Snaps', 'Power AVG RYOE', 'Power AVG EPA',
        'Power Conversion Rate'
        ]
-image = Image.open(r"https://thumbs.gfycat.com/OrderlyKnobbyGoldeneye-size_restricted.gif")
+image_url = "https://thumbs.gfycat.com/OrderlyKnobbyGoldeneye-size_restricted.gif"
+image = Image.open(image_url)
 st.image(image)
 
 ybox = st.selectbox('Select Y Axis!', texas)
